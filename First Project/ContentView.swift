577d1404cc0e876bcaca8667243ce026c6412c6c
Michael Bai <michaelbai30@gmail.com>
2026-08-09 16:48:27 -0400

    Initial Commit

---
+++ First Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First Project
+//
+//  Created by Michael Bai on 8/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First Project/First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ProjectApp.swift
+//  First Project
+//
+//  Created by Michael Bai on 8/9/26.
+//
+
+import SwiftUI
+
+@main
+struct First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
